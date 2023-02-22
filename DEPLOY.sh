b7c1069eca5e56016c10763d17f129958c841d0e
mb6ockatf <mb6ockatf@relay.firefox.com>
2023-02-22 23:46:10 +0300

    add: deploy script (#7)

---
+++ DEPLOY.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+declare -A dot_files
+dot_files[alacritty.yml]="$HOME/.alacritty.yml"
+dot_files[bashrc]="$HOME/.bashrc"
+dot_files[colors.sh]="$HOME/.colors.sh"
+dot_files[settings.json]="$HOME/.config/Code/User/settings.json"
+dot_files[vimrc]="$HOME/.vim/vimrc"
+if [ "$1" == "pack" ]; then
+	for filename in ${!dot_files[@]}; do
+		cp --verbose $"${dot_files[$filename]}" "$filename"
+	done
+	echo "successfully packed to" `pwd`
+elif [ "$1" == "install" ]; then
+	for filename in ${!dot_files[@]}; do
+		cp --verbose "$filename" "${dot_files[$filename]}"
+	done
+	echo "successfully installed in $HOME"
+fi
