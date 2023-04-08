4a33ed9eda254c9d2c38a8ef14d9b29923ce2638
mb6ockatf <mdddmmmm@yandex.ru>
2023-04-08 14:32:22 +0300

    feat(aliases.sh): simple git aliases

--- bash_files/aliases.sh
+++ bash_files/aliases.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-#
-# ~/.bash_files/bash-aliases.sh
-#
+# ~/.bash_files/aliases.sh
+# Sat 08 Apr 2023 01:52:12 PM MSK
 
 alias cp='cp -i'
 alias df='df -h'
@@ -16,4 +15,9 @@ alias py='python3'
 alias pip='python3 -m pip'
 alias vi='vim'
 alias google-chrome='/usr/bin/google-chrome-stable %U'
+alias status='git status'
+# Note that "commit" command is controlled in ~/.bash_files/functions.sh by gum
+alias pull='git pull'
+alias push='git push'
+alias add='git add'
 
