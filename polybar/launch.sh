593bcf63bd006d4f7f3147d0ad25fab1a3793c53
mb6ockatf <mdddmmmm@yandex.ru>
2023-05-01 23:11:49 +0300

    feat(alacritty): change font to iosevka

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 killall -q polybar
 echo "---" | tee -a /tmp/polybar.log
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+polybar main 2>&1 | tee -a /tmp/polybar.log & disown
 
