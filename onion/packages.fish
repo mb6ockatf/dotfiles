f23a91ce0a6cd8f843022efa0eeeb78379bcee8b
mb6ockatf <mdddmmmm@yandex.ru>
2024-04-01 21:36:48 +0300

    feat(bashrc): check internet connection
    also, `packages.fish` file

--- onion/packages.fish
+++ onion/packages.fish
@@ -9,7 +9,7 @@ rm -frd yay-bin
 
 yay -Syuu vim sxhkd bspwm rofi polybar
 yay -Syuu python3 fish google-chrome kitty less
-
+yay -Syuu fortune-mod cowsay
 # https://github.com/junegunn/vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
