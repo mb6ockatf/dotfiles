356debfdd606032f033044311fad8784e8f2c489
mb6ockatf <mdddmmmm@yandex.ru>
2024-04-10 19:24:08 +0300

    feat: packages

--- onion/packages.fish
+++ onion/packages.fish
@@ -7,9 +7,9 @@ makepkg -si
 cd ..
 rm -frd yay-bin
 
-yay -Syuu vim sxhkd bspwm rofi polybar
+yay -Syuu vim sxhkd bspwm rofi polybar util-linux
 yay -Syuu python3 fish google-chrome kitty less
-yay -Syuu fortune-mod cowsay
+yay -Syuu fortune-mod cowsay github-cli libreoffice-still
 # https://github.com/junegunn/vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
